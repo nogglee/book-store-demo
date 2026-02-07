a767d775854353c9f3d8c7c995615892a11227d3
nogglee <dev.nogglee@gmail.com>
2026-02-07 13:35:07 +0900

    feat: connect db

---
+++ mariadb.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createConnection
+({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'Bookstore',
+    dataString: true
+});
+
+connection.connect
+(
+    err => 
+    {
+        if (err) { console.error('❌ DB 연결 실패:', err); return; }
+        else { console.log('✅ DB 연결 성공'); }
+    }
+);
+
+module.exports = connection;
