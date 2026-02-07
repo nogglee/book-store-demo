db6102f2c7805d90752abf6599f2ef51f7b458c9
nogglee <dev.nogglee@gmail.com>
2026-02-07 13:35:32 +0900

    feat: add userRoute

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 app.listen(process.env.PORT, console.log(`🚀 ${process.env.PORT} 포트에서 서버 구동 중!`));
+
+const userRouter = require('./routes/users')
+
+app.use('/users', userRouter);
