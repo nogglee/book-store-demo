89446e1a98f923d4a5ca3a46630673399add0380
nogglee <dev.nogglee@gmail.com>
2026-02-07 18:57:51 +0900

    refactor: modularize business logic into controllers

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const conn = require('../mariadb');
-const handleDbError = require('../utils/handleDbError');
 const validate = require('../middleware/validate');
 const { body, param } = require('express-validator');
-const { StatusCodes } = require('http-status-codes');
+
+const { signup, signin, requestPasswordReset, passwordReset } = require('../controllers/user.controller')
+
 
 router.use(express.json());
 
@@ -15,60 +15,24 @@ router.route('/signup')
             body('email').trim().notEmpty().withMessage('이메일을 입력해 주세요.').isEmail().withMessage('이메일 형식이 올바르지 않습니다.'),
             body('name').trim().notEmpty().withMessage('이름을 입력해 주세요.'),
             body('password').trim().notEmpty().withMessage('비밀번호를 입력해 주세요.'),
-            validate
         ],
-
-        (req, res) =>
-        {
-            const {email, name, password} = req.body
-
-            conn.query
-            (
-                'SELECT * FROM users WHERE email = ?', [email],
-                (err, results) => 
-                {
-                    if (handleDbError(res, err)) return;
-                    if(results.length){ return res.status(StatusCodes.CONFLICT).json({ message : '이미 가입된 이메일입니다.' }) }
-
-                    conn.query
-                    (
-                        'INSERT INTO users (email, name, password) VALUES (?, ?, ?)', [email, name, password],
-                        (err, results) => 
-                        {
-                            if (handleDbError(res, err)) return;
-                            else { return res.status(StatusCodes.CREATED).json({ message : '회원가입에 성공했습니다!' }) }
-                        }
-                    )
-                }
-            )
-        }
+        validate,
+        signup
     );
 
 router.route('/signin')
     .post
-    (
-        '/signin',
-        (req, res) =>
-        {
-            res.json('로그인')
-        }
+    ( 
+        [
+            body('email').trim().notEmpty().withMessage('이메일을 입력해 주세요.').isEmail().withMessage('이메일 형식이 올바르지 않습니다.'),
+            body('password').trim().notEmpty().withMessage('비밀번호를 입력해 주세요.'),
+        ],
+        validate,
+        signin 
     );
 
-
 router.route('/reset')
-    .post // request init password
-    (
-        (req, res) =>
-        {
-            res.json('비밀번호 초기화 요청')
-        }
-    )
-    .put // init password
-    (
-        (req, res) =>
-        {
-            res.json('비밀번호 초기화')
-        }
-    )
+    .post(requestPasswordReset)
+    .put(passwordReset)
 
 module.exports = router;
