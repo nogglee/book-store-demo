836035f3208c5b05bd2b46661b00c45c581a9619
nogglee <dev.nogglee@gmail.com>
2026-02-08 20:46:30 +0900

    feat: add router for books and category

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ dotenv.config();
 
 app.listen(process.env.PORT, console.log(`🚀 ${process.env.PORT} 포트에서 서버 구동 중!`));
 
-const userRouter = require('./routes/users')
+const userRouter = require('./routes/users');
+const bookRouter = require('./routes/books');
+const categoryRouter = require('./routes/category');
+
+app.use
+(
+    (req, res, next) => 
+    {
+        console.log('⚡️ 요청이 들어왔어요! :', req.method, JSON.stringify(req.originalUrl));
+        next();
+    }
+);
 
 app.use('/users', userRouter);
+app.use('/books', bookRouter);
+app.use('/category', categoryRouter);
