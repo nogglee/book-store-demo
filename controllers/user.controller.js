9a8fc49eae4bf687b92ad7f6bf6325a34df11633
nogglee <dev.nogglee@gmail.com>
2026-02-08 11:06:35 +0900

    refactor: move mariadb connection to db folder

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const conn = require('../mariadb');
+const conn = require('../db/mariadb');
 const handleDbError = require('../utils/handleDbError');
 const { StatusCodes } = require('http-status-codes');
 const crypto = require('crypto');
