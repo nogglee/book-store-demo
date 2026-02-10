a3af30cef7c48fb9d977d5214082d3f8ffc3ff6c
nogglee <dev.nogglee@gmail.com>
2026-02-10 12:27:45 +0900

    feat: add business logic gor likes

---
+++ controllers/like.controller.js
@@ -0,0 +1,39 @@
+const conn = require('../db/mariadb');
+const handleDbError = require('../utils/handleDbError');
+const { StatusCodes } = require('http-status-codes');
+
+const addLike = (req, res) => 
+{
+    const liked_book_id = parseInt(req.params.id);
+    const { user_id } = req.body;
+
+    conn.query
+    (
+        'INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)', [user_id, liked_book_id],
+        (err, results) => 
+        {
+            if(handleDbError(res, err)) return;
+
+            return res.status(StatusCodes.OK).json({ message : '좋아요가 등록되었습니다.' })
+        }
+    )
+}
+
+const removeLike = (req, res) => 
+{
+    const liked_book_id = parseInt(req.params.id);
+    const { user_id } = req.body;
+
+    conn.query
+    (
+        'DELETE FROM likes WHERE user_id = ? AND liked_book_id = ?', [user_id, liked_book_id],
+        (err, results) => 
+        {
+            if(handleDbError(res, err)) return;
+
+            return res.status(StatusCodes.OK).json({ message : '좋아요가 삭제되었습니다.' })
+        }
+    )
+}
+
+module.exports = { addLike, removeLike };
