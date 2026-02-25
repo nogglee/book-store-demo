d91c520e56ea2354f6ad449f873eb937e2fee765
nogglee <dev.nogglee@gmail.com>
2026-02-26 00:05:14 +0900

    refactor: order controller에 async/await 적용

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,55 +1,46 @@
-const conn = require('../db/mariadb');
+const mariadb = require('mysql2/promise');
 const { StatusCodes } = require('http-status-codes');
-const handleDbError = require('../utils/handleDbError');
 
-// TODO: 비동기 처리
-const order = (req, res) => 
+const order = async (req, res) => 
 {
-    const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
-
-    var delivery_id;
-    var order_id;
-    let sql = 'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
-    let values = [ delivery.address, delivery.receiver, delivery.contact ];
-
-    conn.query
-    (
-        sql, values,    
-        (err, results) => 
-        {
-            if(handleDbError(res, err)) return;
-            delivery_id = results.insertId;
-        }
-    )
-
-    console.log(delivery_id)
-
-    sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
-    values = [ firstBookTitle, totalQuantity, totalPrice, userId, delivery_id ];
-    conn.query
-    (
-        sql, values,    
-        (err, results) => 
-        {
-            if(handleDbError(res, err)) return;
-            order_id = results.insertId;
-        }
-    )
-
-    sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+    try
+    {
+        const conn = await mariadb.createConnection
+        ({
+            host: 'localhost',
+            user: 'root',
+            password: process.env.DB_PASSWORD,
+            database: 'Bookstore',
+            dataString: true
+        });
     
-    values = [];
-    items.forEach((item) => { values.push([ order_id, item.bookId, item.quantity ]) })
-
-    conn.query
-    (
-        sql, [values],    
-        (err, results) => 
-        {
-            if(handleDbError(res, err)) return;
-            return res.status(StatusCodes.OK).json(results)
-        }
-    )
+        const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } = req.body;
+    
+        let delivery_id;
+        let order_id;
+        let results;
+        
+        let sql = 'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
+        let values = [ delivery.address, delivery.receiver, delivery.contact ];
+    
+        [results] = await conn.query(sql, values);
+        delivery_id = results.insertId;
+    
+        sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
+        values = [ firstBookTitle, totalQuantity, totalPrice, userId, delivery_id ];
+        
+        [results] = await conn.query(sql, values);
+        order_id = results.insertId;
+    
+        sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+        values = [];
+        
+        items.forEach((item) => { values.push([ order_id, item.bookId, item.quantity ]) })
+        [results] = await conn.query(sql, [values]);
+        
+        return res.status(StatusCodes.OK).json(results)
+    }
+    catch (err) { console.error(err); res.status(StatusCodes.BAD_REQUEST).json({ message: 'DB 오류 발생' }); }
 }
 
 const getOrders = (req, res) => 
