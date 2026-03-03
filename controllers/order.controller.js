1a203f0e37fc33cbab00ff1c4574a4d7941f73ab
nogglee <dev.nogglee@gmail.com>
2026-03-03 22:38:30 +0900

    feat: 주문하기 API 수정 및 주문내역 조회 기능 추가

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,5 +1,6 @@
 const mariadb = require('mysql2/promise');
 const { StatusCodes } = require('http-status-codes');
+const connection = require('../db/mariadb');
 
 const order = async (req, res) => 
 {
@@ -23,34 +24,90 @@ const order = async (req, res) =>
         let sql = 'INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)';
         let values = [ delivery.address, delivery.receiver, delivery.contact ];
     
-        [results] = await conn.query(sql, values);
+        [results] = await conn.execute(sql, values);
         delivery_id = results.insertId;
     
         sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)`;
         values = [ firstBookTitle, totalQuantity, totalPrice, userId, delivery_id ];
         
-        [results] = await conn.query(sql, values);
+        [results] = await conn.execute(sql, values);
         order_id = results.insertId;
     
+        // req.body.item를 활용하여 장바구니에서 book_id, quantity 조회
+        sql = `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`;
+        let [orderItems, fields] = await conn.query(sql, [items]);
+
+        // orderedBook 삽입
         sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
         values = [];
-        
-        items.forEach((item) => { values.push([ order_id, item.bookId, item.quantity ]) })
-        [results] = await conn.query(sql, [values]);
-        
-        return res.status(StatusCodes.OK).json(results)
+
+        orderItems.forEach((item) => { values.push([ order_id, item.book_id, item.quantity ]) })
+        results = await conn.query(sql, [values]);
+
+        deleteCartItems(conn, items);
+        return res.status(StatusCodes.OK).json(results[0])
     }
-    catch (err) { console.error(err); res.status(StatusCodes.BAD_REQUEST).json({ message: 'DB 오류 발생' }); }
+    catch (err) { console.error(err); res.status(StatusCodes.BAD_REQUEST).json({ message: 'DB 오류 발생' }) }
 }
 
-const getOrders = (req, res) => 
+const deleteCartItems = async (conn, items) =>
 {
-    res.json('주문 목록 조회')
+    let sql = 'DELETE FROM cartItems WHERE id IN (?)';
+
+    let result = await conn.query(sql, [items]);
+    return result;
 }
 
-const getOrderDetail = (req, res) => 
+const getOrders = async (req, res) => 
 {
-    res.json('주문 상세 조회')
+    try
+    {
+        const conn = await mariadb.createConnection
+        ({
+            host: 'localhost',
+            user: 'root',
+            password: process.env.DB_PASSWORD,
+            database: 'Bookstore',
+            dataString: true
+        });
+        
+        const { userId } = req.body;
+
+        let sql = `SELECT orders.id, created_at, address, receiver, contact, book_title, total_quantity, total_price
+                    FROM orders LEFT JOIN delivery
+                    ON orders.delivery_id = delivery.id`;
+
+        let [rows, fields] = await conn.execute(sql);
+        return res.status(StatusCodes.OK).json(rows);
+    }
+    catch (err) { console.error(err); res.status(StatusCodes.BAD_REQUEST).json({ message: 'DB 오류 발생' }) }
+}
+
+const getOrderDetail = async (req, res) => 
+{
+    try
+    {
+        const conn = await mariadb.createConnection
+        ({
+            host: 'localhost',
+            user: 'root',
+            password: process.env.DB_PASSWORD,
+            database: 'Bookstore',
+            dataString: true
+        });
+        
+        const { id } = req.params;
+        console.log(id);
+
+        let sql = `SELECT book_id, title, author, price, quantity
+                    FROM orderedBook LEFT JOIN books
+                    ON orderedBook.book_id = books.id
+                    WHERE order_id = ?`;
+
+        let [rows, fields] = await conn.execute(sql, [id]);
+        return res.status(StatusCodes.OK).json(rows);
+    }
+    catch (err) { console.error(err); res.status(StatusCodes.BAD_REQUEST).json({ message: 'DB 오류 발생' }) }
 }
 
 module.exports = { order, getOrders, getOrderDetail };
