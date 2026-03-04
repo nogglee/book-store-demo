ffc3eaad95ead5e6004bf63a2a2120a469013a28
nogglee <dev.nogglee@gmail.com>
2026-03-04 19:20:08 +0900

    refactor: JWT 인증 로직을 util에서 middleware로 분리

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,15 +1,15 @@
 const conn = require('../db/mariadb');
-const ensureAuthorization = require('../utils/ensureAutorization');
 const { StatusCodes } = require('http-status-codes');
 const handleDbError = require('../utils/handleDbError');
 
 const addToCart = (req, res) => 
 {
     const { book_id, quantity } = req.body;
+    const user_id = req.user.id;
 
     conn.query
     (
-        'INSERT INTO cartItems (user_id, book_id, quantity) VALUES (?, ?, ?)', [ensureAuthorization(req).id, book_id, quantity],
+        'INSERT INTO cartItems (user_id, book_id, quantity) VALUES (?, ?, ?)', [user_id, book_id, quantity],
         (err, results) => 
         {
             if(handleDbError(res, err)) return;
@@ -22,13 +22,14 @@ const addToCart = (req, res) =>
 const getCartItems = (req, res) =>
 {
     const { selected } = req.body;
+    const user_id = req.user.id;
 
     conn.query
     (
         `SELECT cartItems.id, book_id, title, summary, quantity, price 
         FROM cartItems 
             LEFT JOIN books ON cartItems.book_id = books.id 
-        WHERE user_id = ? AND cartItems.id IN (?)`, [ensureAuthorization(req).id, selected ],
+        WHERE user_id = ? AND cartItems.id IN (?)`, [user_id, selected ],
         (err, results) =>
         {
             if(handleDbError(res, err)) return;
