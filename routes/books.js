5969b52c123e3388de42cb12fdf63360345e4de8
nogglee <dev.nogglee@gmail.com>
2026-03-05 18:53:58 +0900

    [#1] refactor: 라우트에 인증 미들웨어 적용

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const { allBooks, bookDetail } = require('../controllers/book.controller');
 
 router.use(express.json());
 
 router.get('/', allBooks)
-router.get('/:id', bookDetail)
+router.get('/:id', auth, bookDetail)
 
 module.exports = router;
